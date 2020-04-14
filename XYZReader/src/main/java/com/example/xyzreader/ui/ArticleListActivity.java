992a4763072165bb2c2736f5c31c732eb913224e
doilio <doiliomatsinhe@hotmail.com>
2020-04-14 05:59:56 +0200

    Update activity_article_list
    Signed-off-by: doilio <doiliomatsinhe@hotmail.com>

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -59,7 +59,7 @@ public class ArticleListActivity extends AppCompatActivity implements
         mToolbar = findViewById(R.id.toolbar);
 
 
-        final View toolbarContainerView = findViewById(R.id.toolbar_container);
+        //final View toolbarContainerView = findViewById(R.id.toolbar_container);
 
         mSwipeRefreshLayout =  findViewById(R.id.swipe_refresh_layout);
 
